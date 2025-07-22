1b32f8f6160efd5f4534fd39e02c355a7c7c5502
Harsha <you@example.com>
2025-07-22 21:13:36 +0530

    fix

--- src/Common/ThemeToggler/styledComponents.ts
+++ src/Common/ThemeToggler/styledComponents.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const ThemeButton = styled.button`
   font-size:30px;
   margin-top:6px;
-  color:${({theme})=>theme.isDark?'white':''};
+  color:${({theme})=>theme.isDark?'white':'black'};
   border: 0;
   background-color: transparent;
   @media screen and (min-width: 577px) and (max-width: 768px) {
